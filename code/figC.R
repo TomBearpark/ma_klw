3cc0147b56c907ef9eb1b4580b9b076cd27f2574
TomBearpark <t.bearpark@googlemail.com>
2024-08-21 12:48:03 +0100

    extra docs

--- code/figC.R
+++ code/figC.R
@@ -1,1 +1,5 @@
 # This should be a python file i guess?
+# Needs to spit out: 
+  # Data for fig 1c
+  # 2100 confidence intervals for version in which we cluster by country, and 
+  # do a standard version of a bootstrap
