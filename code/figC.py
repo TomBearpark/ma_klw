9b423619f0516bdba06f1344d13ab706750d842c
Dylan Hogan <dth2133@columbia.edu>
2024-08-23 16:54:24 -0400

    Small cleanup

--- code/figC.py
+++ code/figC.py
@@ -1,9 +1,3 @@
-# This should be a python file i guess?
-# Needs to spit out:
-# Data for fig 1c
-# 2100 confidence intervals for version in which we cluster by country, and
-# do a standard version of a bootstrap
-
 import pandas as pd
 import numpy as np
 import os
